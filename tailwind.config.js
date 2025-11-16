21c20988eea5f354dec542040004af856914a48c
Saleemahhh1 <202877436+Saleemahhh1@users.noreply.github.com>
2025-11-16 23:27:33 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        apmGreen: '#00C853',
+        apmGold: '#FFD700',
+        bnbYellow: '#F3BA2F',
+        darkBg: '#0a0a0a'
+      }
+    }
+  },
+  plugins: []
+}
