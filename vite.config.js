fb7b23dfc22c68493eae3682242f814024dae3ba
Saleemahhh1 <202877436+Saleemahhh1@users.noreply.github.com>
2025-11-16 18:24:54 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',         // ensure build output folder
+    sourcemap: true,        // optional, helps debug runtime errors
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash].[ext]'
+      }
+    }
+  }
+})
